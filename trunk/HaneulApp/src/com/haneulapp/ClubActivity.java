97ba1b7c08a8793ec1b96d90d30225159bd10e9d
icanspeakenglissh@gmail.com <icanspeakenglissh@gmail.com@26151534-2c92-6ba8-98ac-b1ebaf060647>
2013-12-22 12:52:31 +0000

    biofarm 동아리를 기준하여 reference sheet를 만듬. 앞으로 동아리원들은 동아리 레이아웃 생성시 이 sheet를 참고하길 바람!(내용만 바꿔주기)

--- trunk/HaneulApp/src/com/haneulapp/ClubActivity.java
+++ trunk/HaneulApp/src/com/haneulapp/ClubActivity.java
@@ -1,8 +1,10 @@
 package com.haneulapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class ClubActivity extends Activity {
 
@@ -11,7 +13,10 @@ public class ClubActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_club);
 	}
-
+	public void go_biofarm (View v) {
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.biofarm);
+		}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
