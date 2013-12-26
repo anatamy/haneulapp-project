053d9ae31d2068734c71b8db8392ca66eb5e011b
icanspeakenglissh@gmail.com <icanspeakenglissh@gmail.com@26151534-2c92-6ba8-98ac-b1ebaf060647>
2013-12-26 14:26:47 +0000

    날아가지마라으아아아아

--- trunk/HaneulApp/src/com/haneulapp/SchoolActivity.java
+++ trunk/HaneulApp/src/com/haneulapp/SchoolActivity.java
@@ -1,8 +1,12 @@
 package com.haneulapp;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.PopupWindow;
 
 public class SchoolActivity extends Activity {
 
@@ -10,13 +14,30 @@ public class SchoolActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_school);
-	}
+		
+		 final Button btnOpenPopup = (Button)findViewById(R.id.openpopup_school);
+	        btnOpenPopup.setOnClickListener(new Button.OnClickListener(){
+	        	 public void onClick(View arg0) {
+	        		    LayoutInflater layoutInflater 
+	        		     = (LayoutInflater)getBaseContext()
+	        		      .getSystemService(LAYOUT_INFLATER_SERVICE);  
+	        		    View popupView = layoutInflater.inflate(R.layout.popup, null);  
+	        		             final PopupWindow popupWindow = new PopupWindow(
+	        		               popupView, 
+	        		               LayoutParams.WRAP_CONTENT,  
+	        		                     LayoutParams.WRAP_CONTENT);  
+	        		             
+	        		             Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+	        		             btnDismiss.setOnClickListener(new Button.OnClickListener(){
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.school, menu);
-		return true;
-	}
-
-}
+	        		     @Override
+	        		     public void onClick(View v) {
+	        		      // TODO Auto-generated method stub
+	        		      popupWindow.dismiss();
+	        		     }});
+	        		               
+	        		             popupWindow.showAsDropDown(btnOpenPopup, 50, -30);
+	        		         
+	        		   }});
+	        		    }
+	        		}
