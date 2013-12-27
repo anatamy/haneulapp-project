e95337eb49227da32f2111158345ffb59410a6a0
icanspeakenglissh@gmail.com <icanspeakenglissh@gmail.com@26151534-2c92-6ba8-98ac-b1ebaf060647>
2013-12-27 08:20:52 +0000

    SchoolActivity에 팝업 윈도우 java 실행파일을 넣어놓았으나 실행이 되지 않는 문제 발생. 고치길 바란다.

---
+++ trunk/HaneulApp/src/com/haneulapp/ClubActivity.java
@@ -0,0 +1,90 @@
+package com.haneulapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.PopupWindow;
+
+public class ClubActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_club);
+	
+		
+		//PopupWindow 구현
+		 final Button btnOpenPopup = (Button)findViewById(R.id.openpopup_club);
+	        btnOpenPopup.setOnClickListener(new Button.OnClickListener(){
+	        	 public void onClick(View arg0) {
+	        		    LayoutInflater layoutInflater 
+	        		     = (LayoutInflater)getBaseContext()
+	        		      .getSystemService(LAYOUT_INFLATER_SERVICE);  
+	        		    View popupView = layoutInflater.inflate(R.layout.popup, null);  
+	        		             final PopupWindow popupWindow = new PopupWindow(
+	        		               popupView, 
+	        		               LayoutParams.WRAP_CONTENT,  
+	        		                     LayoutParams.WRAP_CONTENT);  
+	        		             
+	        		             Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
+	        		             
+	        		             btnDismiss.setOnClickListener(new Button.OnClickListener(){
+
+	        		     @Override
+	        		     public void onClick(View v) {
+	        		      // TODO Auto-generated method stub
+	        		      popupWindow.dismiss();
+	        		     }});
+	        		               
+	        		             popupWindow.showAsDropDown(btnOpenPopup, 50, -30);
+	        		         
+	        		   }});
+	        		    }
+
+
+	
+	public void go_biofarm (View v) { 			//바이오팜
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.biofarm);
+		}
+	public void go_caritas (View v) { 			//카리타스
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.caritas);
+		}
+	public void go_volunteather (View v) { 			//교육봉사
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.volunteather);
+		}
+	public void go_archi (View v) { 			//ARCHI
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.archi);
+		}
+	public void go_alchemist (View v) { 			//Alchemist
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.alchemist);
+		}
+	public void go_blue (View v) { 			//BLUE
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.blue);
+		}
+	public void go_onair (View v) { 			//ON AIR
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.onair);
+		}
+	public void go_event (View v) {				//동아리메인
+		Intent myintent = new Intent(this, ClubActivity.class);
+		setContentView(R.layout.activity_club);
+		}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.club, menu);
+		return true;
+	}
+
+}
