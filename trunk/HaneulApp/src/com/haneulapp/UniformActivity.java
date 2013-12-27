bb0ff70f33c6a67c1658876a47cdec94c5bf09d0
icanspeakenglissh <icanspeakenglissh@gmail.com>
2013-12-27 14:06:31 +0000

    ha

--- trunk/HaneulApp/src/com/haneulapp/UniformActivity.java
+++ trunk/HaneulApp/src/com/haneulapp/UniformActivity.java
@@ -15,32 +15,7 @@ public class UniformActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_uniform);
-	
-	 final Button btnOpenPopup = (Button)findViewById(R.id.openpopup_uniform);
-     btnOpenPopup.setOnClickListener(new Button.OnClickListener(){
-     	 public void onClick(View arg0) {
-     		    LayoutInflater layoutInflater 
-     		     = (LayoutInflater)getBaseContext()
-     		      .getSystemService(LAYOUT_INFLATER_SERVICE);  
-     		    View popupView = layoutInflater.inflate(R.layout.popup, null);  
-     		             final PopupWindow popupWindow = new PopupWindow(
-     		               popupView, 
-     		               LayoutParams.WRAP_CONTENT,  
-     		                     LayoutParams.WRAP_CONTENT);  
-     		             
-     		             Button btnDismiss = (Button)popupView.findViewById(R.id.dismiss);
-     		             btnDismiss.setOnClickListener(new Button.OnClickListener(){
-
-     		     @Override
-     		     public void onClick(View v) {
-     		      // TODO Auto-generated method stub
-     		      popupWindow.dismiss();
-     		     }});
-     		               
-     		             popupWindow.showAsDropDown(btnOpenPopup, 50, -30);
-     		         
-     		   }});
-     		    }
+	}
      		
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
